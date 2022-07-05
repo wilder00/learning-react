9281052a72b18b66c3db49bf111e606eabea2156
Wilder Trujillo <nadie437@gmail.com>
2022-07-05 14:47:07 -0500

    manejando propiedades y estado de componente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -20,6 +20,67 @@ function App() {
       </header>
     </div>
   );
+} */
+
+import { Component } from "react";
+
+
+class Button extends Component {
+  state = {
+  }
+  constructor(props){
+    super(props)
+    console.log('constructor', props);
+  }
+
+  
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate", prevProps, prevState );
+  }
+
+  componentWillUnmount(){
+    console.log('Desmontando componente', this.props, this.state);
+  }
+  
+  render() {
+    console.log("Ejecutando método render de button");
+    return(
+      <button onClick={ ()=>this.setState({ edad: 21 }) }>
+        Enviar
+      </button>
+    )
+  }
+}
+
+
+class App extends Component{
+
+  state = {
+    valor: 3
+  }
+
+  //retornar contenido jsx
+  render() {
+    console.log(this.state);
+    return(
+      <div>
+        <p>Hola mundo</p>
+        { this.state.valor === 3
+            ?<Button caminar={true} />
+            : null
+          }
+        <button
+          onClick={ ()=> this.setState({valor: 2}) }
+        >
+          Enviar desde el App
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
