384401d2cfc2870720d22472d1561ca4bef4f309
Wilder Trujillo <nadie437@gmail.com>
2022-07-08 16:02:37 -0500

    agreagando libreria formik y creando un formulario básico

--- src/App.js
+++ src/App.js
@@ -1,86 +1,40 @@
-/* import logo from './logo.svg';
-import './App.css';
+import { useFormik } from "formik";
 
-function App() {
+const App = () => {
+  const formik = useFormik({
+    initialValues:{
+      name: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: values => console.log(values)
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
-
-import { Component } from "react";
-
-
-class Button extends Component {
-  state = {
-  }
-  constructor(props){
-    super(props)
-    console.log('constructor', props);
-  }
-
-  
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate", prevProps, prevState );
-  }
-
-  componentWillUnmount(){
-    console.log('Desmontando componente', this.props, this.state);
-  }
-  
-  render() {
-    console.log("Ejecutando método render de button");
-    return(
-      <button onClick={ ()=>this.setState({ edad: 21 }) }>
-        Enviar
-      </button>
-    )
-  }
-}
-
-
-class App extends Component{
-
-  state = {
-    valor: 3
-  }
-
-  //retornar contenido jsx
-  render() {
-    console.log(this.state);
-    return(
-      <div>
-        <p>Hola mundo</p>
-        { this.state.valor === 3
-            ?<Button caminar={true} />
-            : null
-          }
-        <button
-          onClick={ ()=> this.setState({valor: 2}) }
-        >
-          Enviar desde el App
-        </button>
-      </div>
-    )
-  }
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        values={formik.values.name}
+      />
+      <label>Apellido</label>
+      <input
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        values={formik.values.lastName}
+      />
+      <label>Email</label>
+      <input
+        name="email"
+        type="text"
+        onChange={formik.handleChange}
+        values={formik.values.email}
+      />
+      <button type="submit"> Enviar </button>
+    </form>
+  )
 }
 
 export default App;
