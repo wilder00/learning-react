b3153d2110cae628c2fe51de7dbe9223eb7b3f32
Wilder Trujillo <nadie437@gmail.com>
2022-07-08 11:53:08 -0500

    formulario y rendimiento por componentes

--- src/App.js
+++ src/App.js
@@ -1,86 +1,41 @@
-/* import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import Card from './components/Card'
+import Container from './components/Container'
+import UserForm from "./components/UserForm";
+
+const App = ()=>{
+  const [usuarios, setUsuarios] = useState([])
+  
+
+  const submit = usuario =>{
+    setUsuarios([
+      ...usuarios,
+      usuario,
+    ])
+  }
+
+  console.log(formulario, usuarios);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginTop: '15%'}}>
+      <Container>
+        <Card>
+          <div style={{ padding: 20 }}>
+            <UserForm />
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {usuarios.map(x => 
+              <li key={x.email}>
+                {` ${x.name} ${x.lastname} ${x.email}`}
+              </li>
+            )}
+          </ul>
+        </Card>
+      </Container>
     </div>
-  );
-} */
-
-import { Component } from "react";
-
-
-class Button extends Component {
-  state = {
-  }
-  constructor(props){
-    super(props)
-    console.log('constructor', props);
-  }
-
-  
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate", prevProps, prevState );
-  }
-
-  componentWillUnmount(){
-    console.log('Desmontando componente', this.props, this.state);
-  }
-  
-  render() {
-    console.log("Ejecutando método render de button");
-    return(
-      <button onClick={ ()=>this.setState({ edad: 21 }) }>
-        Enviar
-      </button>
-    )
-  }
-}
-
-
-class App extends Component{
-
-  state = {
-    valor: 3
-  }
-
-  //retornar contenido jsx
-  render() {
-    console.log(this.state);
-    return(
-      <div>
-        <p>Hola mundo</p>
-        { this.state.valor === 3
-            ?<Button caminar={true} />
-            : null
-          }
-        <button
-          onClick={ ()=> this.setState({valor: 2}) }
-        >
-          Enviar desde el App
-        </button>
-      </div>
-    )
-  }
+  )
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import './Card.css'
+
+
+const Card = ({ children })=>{
+  
+  return(
+    <div className="card">
+      { children }
+    </div>
+  )
+}
+
+export default Card
