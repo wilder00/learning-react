3f90c6d3513f59c239a8da48eff35e140f86de30
Wilder Trujillo <nadie437@gmail.com>
2022-07-20 06:59:16 -0500

    creando un app de buscador de imagenes

--- src/App.js
+++ src/App.js
@@ -1,86 +1,45 @@
-/* import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from 'formik'
+import { useState } from 'react'
+import './header.css'
+import './content.css'
+import './article.css'
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{search:''}}
+          onSubmit={ async values=> {
+            console.log(values);
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,{
+              headers: {
+                'Authorization': 'Client-ID kS_vPgZDU3uyny9l1JY15OT8sFWBRVFbXo20T966M70'
+              }
+            })
+            const data = await response.json();
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
-    </div>
-  );
-} */
-
-import { Component } from "react";
-
-
-class Button extends Component {
-  state = {
-  }
-  constructor(props){
-    super(props)
-    console.log('constructor', props);
-  }
-
-  
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate", prevProps, prevState );
-  }
-
-  componentWillUnmount(){
-    console.log('Desmontando componente', this.props, this.state);
-  }
-  
-  render() {
-    console.log("Ejecutando método render de button");
-    return(
-      <button onClick={ ()=>this.setState({ edad: 21 }) }>
-        Enviar
-      </button>
-    )
-  }
-}
-
-
-class App extends Component{
-
-  state = {
-    valor: 3
-  }
-
-  //retornar contenido jsx
-  render() {
-    console.log(this.state);
-    return(
-      <div>
-        <p>Hola mundo</p>
-        { this.state.valor === 3
-            ?<Button caminar={true} />
-            : null
-          }
-        <button
-          onClick={ ()=> this.setState({valor: 2}) }
-        >
-          Enviar desde el App
-        </button>
+      <div className="container">
+          <div className="center">
+            {photos.map(photo =>
+              <article key={photo.id} onClick={()=> open(photo.links.html)}>
+                <img src={photo.urls.regular} />
+                <p> {[photo.description, photo.alt_description].join(' - ')} </p>
+              </article>
+            )}
+          </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App;
