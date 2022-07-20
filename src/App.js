5df0148f690310f45d950ccfa60db02537a941aa
Wilder Trujillo <nadie437@gmail.com>
2022-07-20 05:53:41 -0500

    completando campos de props con formik.getFieldProps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import { useFormik } from "formik";
 
+
+const validate = (values)=>{
+  const errors = {};
+  if(!values.name){
+    errors.name = 'Required'
+  }else if(values.name.length < 5){
+    errors.name = 'El nombre es muy corto'
+  }
+  
+  if(!values.lastName){
+    errors.lastName = 'Required'
+  }else if(values.lastName.length < 5){
+    errors.lastName = 'El apellido es muy corto'
+  }
+  return errors
+}
+
+
 const App = () => {
   const formik = useFormik({
     initialValues:{
@@ -7,31 +25,33 @@ const App = () => {
       lastName: '',
       email: '',
     },
+    validate,
     onSubmit: values => console.log(values)
   })
   return (
     <form onSubmit={formik.handleSubmit}>
       <label>Nombre</label>
       <input
-        name="name"
         type="text"
-        onChange={formik.handleChange}
-        values={formik.values.name}
+        {...formik.getFieldProps('name')}
       />
+      {formik.touched.name && formik.errors.name? <div>{formik.errors.name}</div> : null}
+      <br/>
       <label>Apellido</label>
       <input
-        name="lastName"
         type="text"
-        onChange={formik.handleChange}
-        values={formik.values.lastName}
+        {...formik.getFieldProps('lastname')}
       />
+      {formik.touched.lastName && formik.errors.lastName? <div>{formik.errors.lastName}</div> : null}
+
+      <br/>
       <label>Email</label>
       <input
-        name="email"
         type="text"
-        onChange={formik.handleChange}
-        values={formik.values.email}
+        {...formik.getFieldProps('email')}
       />
+      {formik.touched.lastName && formik.errors.lastName? <div>{formik.errors.lastName}</div> : null}
+      <br/>
       <button type="submit"> Enviar </button>
     </form>
   )
