c4ab7a5ae4cbbf1a3cd54d13e83ae11530ed6df2
Wilder Trujillo <nadie437@gmail.com>
2022-07-20 07:37:19 -0500

    aprendiendo de la creación de componentes estilizados

--- src/App.js
+++ src/App.js
@@ -1,86 +1,43 @@
-/* import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
+const Content = styled.div`
+  padding: 20px 25px;
+`
+
+const Button = styled.button`
+  background-color: ${props => props.primary? 'white' : 'green' };
+  color: ${props => props.primary? 'green' : 'white' };
+  padding: 10px 15px;
+  border: solid 2px green;
+  border-radius: 4px;
+`
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+  background-color: ${props => props.primary? 'white' : '#453967' };
+`
+
+const App = () => {
+  const [isPrimary, setIsPrimary] = useState(false)
+  console.log(
+    isPrimary
   );
-} */
-
-import { Component } from "react";
-
-
-class Button extends Component {
-  state = {
-  }
-  constructor(props){
-    super(props)
-    console.log('constructor', props);
-  }
-
-  
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate", prevProps, prevState );
-  }
-
-  componentWillUnmount(){
-    console.log('Desmontando componente', this.props, this.state);
-  }
-  
-  render() {
-    console.log("Ejecutando método render de button");
-    return(
-      <button onClick={ ()=>this.setState({ edad: 21 }) }>
-        Enviar
-      </button>
-    )
-  }
+  return (
+    <Content>
+      <P>Hola soy un párrafo</P>
+      <Button primary={isPrimary} onClick={e => setIsPrimary(!isPrimary)}> Enviar </Button>
+      <BlockButton primary={!isPrimary} onClick={e => setIsPrimary(!isPrimary)}> Enviar </BlockButton>
+      {/* cambiando la etiqueta con la que se renderiza */}
+      <BlockButton as="a" href="#" primary={!isPrimary} onClick={e => setIsPrimary(!isPrimary)}> Enviar </BlockButton>
+    </Content>
+  )
 }
 
 
-class App extends Component{
-
-  state = {
-    valor: 3
-  }
-
-  //retornar contenido jsx
-  render() {
-    console.log(this.state);
-    return(
-      <div>
-        <p>Hola mundo</p>
-        { this.state.valor === 3
-            ?<Button caminar={true} />
-            : null
-          }
-        <button
-          onClick={ ()=> this.setState({valor: 2}) }
-        >
-          Enviar desde el App
-        </button>
-      </div>
-    )
-  }
-}
 
 export default App;
