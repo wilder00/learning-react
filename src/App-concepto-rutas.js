83f0c8e60ddcad63c3c3c6ef09a7be423b9d859a
Wilder Trujillo <nadie437@gmail.com>
2022-07-26 08:41:47 -0500

    agregando rutas anidadas

---
+++ src/App-concepto-rutas.js
@@ -0,0 +1,39 @@
+import './App.css'
+import {  Routes, Route, Link} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/portafolio">Portafolio</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <section>
+        <Routes>
+          <Route exact path="/" 
+            element={
+              <h1>Inicio</h1>
+            }
+          />
+          <Route path="/portafolio"
+            element={
+              <h1>Portafolio</h1>
+            }
+          />
+          
+        </Routes>
+      </section>
+    </div>
+  )
+}
+
+
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import './App.css'
 import {  Routes, Route, Link} from 'react-router-dom'
 
+const Portafolio = () =>{
+  return (
+    <div>
+      <h1> Portafolio </h1>
+      <ul>
+        <li>
+          <Link to={'/portafolio/proyecto-1'}>Proyecto 1 </Link>
+        </li>
+        <li>
+          <Link to="proyecto-2">Proyecto 2 </Link>
+        </li>
+      </ul>
+
+      <div>
+        <Routes>
+          <Route path="/"
+            element={
+              <h1>Proyecto Inicio</h1>
+            }
+          />
+          <Route path={'proyecto-1'}
+            element={
+              <h1>Proyecto 1</h1>
+            }
+          />
+          
+          <Route path="/proyecto-2"
+            element={
+              <h1>Proyecto 2</h1>
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+
 const App = () => {
   return (
     <div>
@@ -22,9 +60,9 @@ const App = () => {
               <h1>Inicio</h1>
             }
           />
-          <Route path="/portafolio"
+          <Route path="/portafolio/*"
             element={
-              <h1>Portafolio</h1>
+              <Portafolio/>
             }
           />
           
@@ -33,7 +71,5 @@ const App = () => {
     </div>
   )
 }
-
-
 
 export default App;
