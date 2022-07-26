350fb9e1979f9eae66a96498b2802879aaaf169c
Wilder Trujillo <nadie437@gmail.com>
2022-07-26 10:10:10 -0500

    agregando rutas dinámicas

--- src/2-nested.js
+++ src/2-nested.js
@@ -1,35 +1,16 @@
 import './App.css'
-import {  Routes, Route, Link, useMatch, useParams } from 'react-router-dom'
-
-const Proyecto = ()=>{
-  const match = useMatch("/portafolio/:proyecto_id")
-  const params = useParams();
-  const { proyecto_id } = params;
-  console.log("el match: ", match);
-  console.log("el params: ", params );
-  return (
-    <h2> Proyecto id: {proyecto_id}</h2>
-  )
-}
+import {  Routes, Route, Link} from 'react-router-dom'
 
 const Portafolio = () =>{
-  const match = useMatch("/portafolio/*")
-  console.log("match en Portfolio", match);
   return (
     <div>
       <h1> Portafolio </h1>
       <ul>
         <li>
-          <Link to={`${match.pathnameBase}/proyecto-1`}>Proyecto 1 </Link>
+          <Link to={'/portafolio/proyecto-1'}>Proyecto 1 </Link>
         </li>
         <li>
           <Link to="proyecto-2">Proyecto 2 </Link>
-        </li>
-        <li>
-          <Link to="54">Proyecto id 54 </Link>
-        </li>
-        <li>
-          <Link to="105">Proyecto id 105 </Link>
         </li>
       </ul>
 
@@ -49,12 +30,6 @@ const Portafolio = () =>{
           <Route path="/proyecto-2"
             element={
               <h1>Proyecto 2</h1>
-            }
-          />
-          
-          <Route path="/:proyecto_id"
-            element={
-              <Proyecto />
             }
           />
         </Routes>
